1d5c0f62dd1157441cf46f006f08528cca4afe23
Ricky Roy Nardson <rickyroynardson@gmail.com>
2026-08-19 18:58:25 +0700

    feat: project structure

---
+++ apps/api/cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r.Handler(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("server shutdown:", err)
+	}
+	log.Println("server exiting")
+}
